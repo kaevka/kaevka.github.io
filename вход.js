5ede13f793b46dc1031a5ae5bedd078d62f26001
kaevka <105744651+kaevka@users.noreply.github.com>
2022-11-17 23:04:12 +0300

    Add files via upload

---
+++ вход.js
@@ -0,0 +1,14 @@
+let result;
+while(true)
+{
+result = prompt("Желаете ли Вы войти на сайт?");
+if (result == "да")
+{
+alert("Входите");
+break;
+}
+else
+{
+alert("Попробуйте еще раз");
+}
+};
---
+++ корзина_джава.js
@@ -0,0 +1,20 @@
+function liked(){
+    var element = document.getElementById("like");
+    element.classList.toggle("liked");
+    }
+    function liked1(){
+    var element = document.getElementById("like1");
+    element.classList.toggle("liked");
+    }
+    function liked2(){
+    var element = document.getElementById("like2");
+    element.classList.toggle("liked");
+    }
+    function liked3(){
+    var element = document.getElementById("like3");
+    element.classList.toggle("liked");
+    }
+    function liked4(){
+        var element = document.getElementById("like4");
+        element.classList.toggle("liked");
+        }
